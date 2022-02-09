1d90eb8410e582cbdbd1bef56c939b85e301bb50
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-02-09 10:58:24 +0200

    fixing a glich in next auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -34,7 +34,7 @@ const Header = ()=>{
             </p>
             <p className="font-extrabold text-xs md:text-sm">{'Account & Lists'}</p>
           </div>
-          <div className="link">
+          <div className="link" onClick={()=> router.push('/orders')}>
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">{'& Orders'}</p>
           </div>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,12 @@ import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
+    secret : process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
